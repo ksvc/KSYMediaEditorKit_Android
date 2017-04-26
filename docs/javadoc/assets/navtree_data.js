68253ebf6ab8c4d8fa2bda5d80731403a522e1b7
zanxiaofei <zanxiaofei@kingsoft.com>
2017-04-26 12:28:54 +0800

    add doces

---
+++ docs/javadoc/assets/navtree_data.js
@@ -0,0 +1,11 @@
+var NAVTREE_DATA =
+[ [ "com.ksyun.media.shortvideo.kit", "com/ksyun/media/shortvideo/kit/package-summary.html", [ [ "Interfaces", null, [ [ "KSYEditKit.OnErrorListener", "com/ksyun/media/shortvideo/kit/KSYEditKit.OnErrorListener.html", null, "" ], [ "KSYEditKit.OnInfoListener", "com/ksyun/media/shortvideo/kit/KSYEditKit.OnInfoListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "KSYEditKit", "com/ksyun/media/shortvideo/kit/KSYEditKit.html", null, "" ], [ "KSYRecordKit", "com/ksyun/media/shortvideo/kit/KSYRecordKit.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.ksyun.media.shortvideo.utils", "com/ksyun/media/shortvideo/utils/package-summary.html", [ [ "Interfaces", null, [ [ "AuthInfoManager.CheckAuthResultListener", "com/ksyun/media/shortvideo/utils/AuthInfoManager.CheckAuthResultListener.html", null, "" ], [ "KS3ClientWrap.OnGetAuthInfoListener", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.OnGetAuthInfoListener.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AuthInfoManager", "com/ksyun/media/shortvideo/utils/AuthInfoManager.html", null, "" ], [ "AuthInfoManager.AuthInfo", "com/ksyun/media/shortvideo/utils/AuthInfoManager.AuthInfo.html", null, "" ], [ "KS3ClientWrap", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.html", null, "" ], [ "KS3ClientWrap.KS3AuthInfo", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.KS3AuthInfo.html", null, "" ], [ "KS3ClientWrap.KS3UploadInfo", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.KS3UploadInfo.html", null, "" ], [ "ShortVideoConstants", "com/ksyun/media/shortvideo/utils/ShortVideoConstants.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ docs/javadoc/reference/lists.js
@@ -0,0 +1,17 @@
+var DATA = [
+      { id:0, label:"com.ksyun.media.shortvideo.kit", link:"com/ksyun/media/shortvideo/kit/package-summary.html", type:"package" },
+      { id:1, label:"com.ksyun.media.shortvideo.kit.KSYEditKit", link:"com/ksyun/media/shortvideo/kit/KSYEditKit.html", type:"class" },
+      { id:2, label:"com.ksyun.media.shortvideo.kit.KSYEditKit.OnErrorListener", link:"com/ksyun/media/shortvideo/kit/KSYEditKit.OnErrorListener.html", type:"class" },
+      { id:3, label:"com.ksyun.media.shortvideo.kit.KSYEditKit.OnInfoListener", link:"com/ksyun/media/shortvideo/kit/KSYEditKit.OnInfoListener.html", type:"class" },
+      { id:4, label:"com.ksyun.media.shortvideo.kit.KSYRecordKit", link:"com/ksyun/media/shortvideo/kit/KSYRecordKit.html", type:"class" },
+      { id:5, label:"com.ksyun.media.shortvideo.utils", link:"com/ksyun/media/shortvideo/utils/package-summary.html", type:"package" },
+      { id:6, label:"com.ksyun.media.shortvideo.utils.AuthInfoManager", link:"com/ksyun/media/shortvideo/utils/AuthInfoManager.html", type:"class" },
+      { id:7, label:"com.ksyun.media.shortvideo.utils.AuthInfoManager.AuthInfo", link:"com/ksyun/media/shortvideo/utils/AuthInfoManager.AuthInfo.html", type:"class" },
+      { id:8, label:"com.ksyun.media.shortvideo.utils.AuthInfoManager.CheckAuthResultListener", link:"com/ksyun/media/shortvideo/utils/AuthInfoManager.CheckAuthResultListener.html", type:"class" },
+      { id:9, label:"com.ksyun.media.shortvideo.utils.KS3ClientWrap", link:"com/ksyun/media/shortvideo/utils/KS3ClientWrap.html", type:"class" },
+      { id:10, label:"com.ksyun.media.shortvideo.utils.KS3ClientWrap.KS3AuthInfo", link:"com/ksyun/media/shortvideo/utils/KS3ClientWrap.KS3AuthInfo.html", type:"class" },
+      { id:11, label:"com.ksyun.media.shortvideo.utils.KS3ClientWrap.KS3UploadInfo", link:"com/ksyun/media/shortvideo/utils/KS3ClientWrap.KS3UploadInfo.html", type:"class" },
+      { id:12, label:"com.ksyun.media.shortvideo.utils.KS3ClientWrap.OnGetAuthInfoListener", link:"com/ksyun/media/shortvideo/utils/KS3ClientWrap.OnGetAuthInfoListener.html", type:"class" },
+      { id:13, label:"com.ksyun.media.shortvideo.utils.ShortVideoConstants", link:"com/ksyun/media/shortvideo/utils/ShortVideoConstants.html", type:"class" }
+
+    ];
