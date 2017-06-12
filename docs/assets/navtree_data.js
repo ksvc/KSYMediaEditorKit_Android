cd58ef58c8e95624de0629de84b29860444046f4
zanxiaofei <zanxiaofei@kingsoft.com>
2017-06-12 12:24:25 +0800

    update doc

--- docs/assets/navtree_data.js
+++ docs/assets/navtree_data.js
@@ -1,9 +1,11 @@
 var NAVTREE_DATA =
-[ [ "com.ksyun.media.shortvideo.kit", "com/ksyun/media/shortvideo/kit/package-summary.html", [ [ "Interfaces", null, [ [ "KSYEditKit.OnErrorListener", "com/ksyun/media/shortvideo/kit/KSYEditKit.OnErrorListener.html", null, "" ], [ "KSYEditKit.OnInfoListener", "com/ksyun/media/shortvideo/kit/KSYEditKit.OnInfoListener.html", null, "" ] ]
+[ [ "com.ksyun.media.shortvideo.kit", "com/ksyun/media/shortvideo/kit/package-summary.html", [ [ "Interfaces", null, [ [ "KSYEditKit.OnErrorListener", "com/ksyun/media/shortvideo/kit/KSYEditKit.OnErrorListener.html", null, "" ], [ "KSYEditKit.OnInfoListener", "com/ksyun/media/shortvideo/kit/KSYEditKit.OnInfoListener.html", null, "" ], [ "KSYRecordKit.MegerFilesFinishedListener", "com/ksyun/media/shortvideo/kit/KSYRecordKit.MegerFilesFinishedListener.html", null, "" ] ]
 , "" ], [ "Classes", null, [ [ "KSYEditKit", "com/ksyun/media/shortvideo/kit/KSYEditKit.html", null, "" ], [ "KSYRecordKit", "com/ksyun/media/shortvideo/kit/KSYRecordKit.html", null, "" ] ]
 , "" ] ]
 , "" ], [ "com.ksyun.media.shortvideo.utils", "com/ksyun/media/shortvideo/utils/package-summary.html", [ [ "Interfaces", null, [ [ "AuthInfoManager.CheckAuthResultListener", "com/ksyun/media/shortvideo/utils/AuthInfoManager.CheckAuthResultListener.html", null, "" ], [ "KS3ClientWrap.OnGetAuthInfoListener", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.OnGetAuthInfoListener.html", null, "" ] ]
 , "" ], [ "Classes", null, [ [ "AuthInfoManager", "com/ksyun/media/shortvideo/utils/AuthInfoManager.html", null, "" ], [ "AuthInfoManager.AuthInfo", "com/ksyun/media/shortvideo/utils/AuthInfoManager.AuthInfo.html", null, "" ], [ "KS3ClientWrap", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.html", null, "" ], [ "KS3ClientWrap.KS3AuthInfo", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.KS3AuthInfo.html", null, "" ], [ "KS3ClientWrap.KS3UploadInfo", "com/ksyun/media/shortvideo/utils/KS3ClientWrap.KS3UploadInfo.html", null, "" ], [ "ShortVideoConstants", "com/ksyun/media/shortvideo/utils/ShortVideoConstants.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.ksyun.media.shortvideo.view", "com/ksyun/media/shortvideo/view/package-summary.html", [ [ "Classes", null, [ [ "KSYStickerView", "com/ksyun/media/shortvideo/view/KSYStickerView.html", null, "" ], [ "StickerHelpBoxInfo", "com/ksyun/media/shortvideo/view/StickerHelpBoxInfo.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
